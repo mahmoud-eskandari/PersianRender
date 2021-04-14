11a712fc31cd8a72edb30a4b55759581d6f0ab0e
Δli <fakhamati@gmail.com>
2021-04-14 19:11:35 +0430

    Fix remove Latin words

--- lib/PersianRender.php
+++ lib/PersianRender.php
@@ -60,7 +60,62 @@ class PersianRender {
         'ؤ' => ['ﺆ', 'ﺆ', ''],
         'إ' => ['ﺈ', 'ﺈ', 'ﺇ'],
         'ئ' => ['ﺊ', 'ﺌ', 'ﺋ'],
-        'ة' => ['ﺔ', 'ﺘ', 'ﺗ'],
+        'ة' => ['ﺔ', 'ﺘ', 'ﺗ']
+    ];
+
+    private static $latinAlphabet = [
+        'A' => 'A',
+        'B' => 'B',
+        'C' => 'C',
+        'D' => 'D',
+        'E' => 'E',
+        'F' => 'F',
+        'G' => 'G',
+        'H' => 'H',
+        'I' => 'I',
+        'J' => 'J',
+        'K' => 'K',
+        'L' => 'L',
+        'M' => 'M',
+        'N' => 'N',
+        'O' => 'O',
+        'P' => 'P',
+        'Q' => 'Q',
+        'R' => 'R',
+        'S' => 'S',
+        'T' => 'T',
+        'U' => 'U',
+        'V' => 'V',
+        'W' => 'W',
+        'X' => 'X',
+        'Y' => 'Y',
+        'Z' => 'Z',
+        'a' => 'a',
+        'b' => 'b',
+        'c' => 'c',
+        'd' => 'd',
+        'e' => 'e',
+        'f' => 'f',
+        'g' => 'g',
+        'h' => 'h',
+        'i' => 'i',
+        'j' => 'j',
+        'k' => 'k',
+        'l' => 'l',
+        'm' => 'm',
+        'n' => 'n',
+        'o' => 'o',
+        'p' => 'p',
+        'q' => 'q',
+        'r' => 'r',
+        's' => 's',
+        't' => 't',
+        'u' => 'u',
+        'v' => 'v',
+        'w' => 'w',
+        'x' => 'x',
+        'y' => 'y',
+        'z' => 'z'
     ];
 
     /**
@@ -101,7 +156,7 @@ class PersianRender {
         '8' => '۸',
         '۸' => '۸',
         '9' => '۹',
-        '۹' => '۹',
+        '۹' => '۹'
     ];
 
     private static $numbers = [
@@ -136,6 +191,18 @@ class PersianRender {
                 $hold[] = $str[$i];
                 $i++;
                 if (!isset(self::$numbers[$str[$i]])) {
+                    $hold = array_reverse($hold);
+                    for ($h = 0; $h < count($hold); $h++) {
+                        $out[] = $hold[$h];
+                    }
+                    $hold = [];
+                }
+                continue;
+            }
+            if (isset(self::$latinAlphabet[$str[$i]])) {
+                $hold[] = $str[$i];
+                $i++;
+                if (!isset(self::$latinAlphabet[$str[$i]])) {
                     $hold = array_reverse($hold);
                     for ($h = 0; $h < count($hold); $h++) {
                         $out[] = $hold[$h];
